1f616844cbf17201c3d0dc320006a9a9e71a1d60
gr1zz-sharp <keithinscore@gmail.com>
2022-02-09 16:14:15 -0600

    finish student

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import Student from './components/Student';
+import Toggle from './components/Toggle';
+import Button from './components/Button';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: [{
+        name: 'Bob Ross',
+        img: 'https://media.giphy.com/media/rYEAkYihZsyWs/giphy.gif'
+      },
+      {
+        name: 'Mr. Rogers',
+        img: 'https://media.giphy.com/media/x0npYExCGOZeo/giphy.gif'
+      },
+      {
+        name: 'Carrie Fisher',
+        img: 'https://media.giphy.com/media/CXU8axmXoPHUY/giphy.gif'
+      }
+    ],
+
+    pickFromAll: true,
+    pickedStudent: null
+    }
+  }
+
+  pickRandomStudent = () => {
+    this.setState(() => ({
+      pickedStudent: this.state.students[Math.floor(Math.random() * this.state.students.length)],
+    }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Student student={this.state.pickedStudent} />
+        <button onClick={this.pickRandomStudent}>Pick Student</button>
+        <Toggle />
+      </div>
+    );
+  }
 }
 
 export default App;
